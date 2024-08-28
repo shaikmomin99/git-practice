535552c6f54a1f0860fe7b07fa8c868520e19e66
Momin Shaik <mominshaik999@gmail.com>
2024-08-28 11:04:18 +0530

    developera updated code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 def addition(a,b,c):
     return a+b+c
 
-def substraction(a,b):
-    return a-b
+def substraction(a,b,c):
+    return a-b-c
 
-def division(a,b):
-    return a/b
+def division(a,b,c):
+    return a/b/c
